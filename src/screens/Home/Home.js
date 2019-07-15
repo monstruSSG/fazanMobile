b3591fe01f7174da46d81508a268bec0cd26a48a
monstruSSG <b_giurgi@yahoo.ro>
2019-07-15 19:48:38 +0300

    Styling example

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-import { View, Button, StyleSheet, Text, Image } from 'react-native';
+import { View, Button, StyleSheet, Image } from 'react-native';
 
 import CONSTANTS from '../../utils/constants';
 import Logo from '../../assets/fazanLogo.png';
 import Title from '../../assets/fazanTitle.png';
 
 class HomeScreen extends Component {
+    //Example on how to style React-navigation-topbar
+    static navigationOptions = {
+        title: 'Home',
+        headerStyle: {
+            backgroundColor: '#7b5e20'
+        }
+    }
+
     navigateSingleplayerScreen = () => this.props.navigation.navigate('Singleplayer');
     navigateSearchGameScreen = () => this.props.navigation.navigate('SearchGame');
 
@@ -13,22 +21,22 @@ class HomeScreen extends Component {
         return (
             <View style={styles.homePage}>
                 <View style={styles.logoContainer}>
-                    <Image source={Logo}/>
+                    <Image source={Logo} />
                 </View>
                 <View style={styles.titleContainer}>
-                    <Image source={Title}/>
+                    <Image source={Title} />
                 </View>
 
                 <View style={styles.content}>
-                    <Button color={CONSTANTS.buttonColor} 
-                        title="Singleplayer" 
-                        width={250} 
-                        height={45} 
+                    <Button color={CONSTANTS.buttonColor}
+                        title="Singleplayer"
+                        width={250}
+                        height={45}
                         onPress={this.navigateSingleplayerScreen}>SINGLE PLAYER</Button>
-                    <Button color={CONSTANTS.buttonColor} 
-                        title="Multiplayer" 
-                        width={250} 
-                        height={45} 
+                    <Button color={CONSTANTS.buttonColor}
+                        title="Multiplayer"
+                        width={250}
+                        height={45}
                         onPress={this.navigateSearchGameScreen}>SEARCH GAME</Button>
                 </View>
             </View>
