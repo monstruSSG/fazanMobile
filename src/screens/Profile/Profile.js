ca069a304bb7a5edb871e402c12841260104eab4
monstruSSG <b_giurgi@yahoo.ro>
2019-08-11 02:15:11 +0300

    Added pie chart plus start scripts for ios/android

--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Image } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Pie from 'react-native-pie';
 
-import Button from '../../components/UI/Button/Button';
 import Text from '../../components/UI/Text/Text';
-import Title from '../../assets/fazanTitle.png';
 import CONSTANTS from '../../utils/constants';
 import Header from '../../components/Header/Header';
 
 class ProfileScreen extends Component {
     static navigationOptions = {
-        header: <Header title='My custom header'/>,
+        header: <Header title='My custom header' />,
     }
 
     render() {
         return (
             <View style={styles.content}>
-                <Text>My PROFILE</Text>
+                <View style={styles.gamesData}>
+                    <View style={styles.stats}>
+                        <Pie
+                            radius={50}
+                            innerRadius={45}
+                            series={[60]}
+                            colors={['#f00']}
+                            backgroundColor='#ddd' />
+                    </View>
+                    <View style={styles.points}>
+                        <Text>Point/elo etc</Text>
+                    </View>
+                </View>
+                <View style={styles.lastGames}>
+                    <Text>Game history</Text>
+                </View>
             </View>
         );
     }
@@ -25,8 +38,28 @@ class ProfileScreen extends Component {
 const styles = StyleSheet.create({
     content: {
         flex: 1,
+        justifyContent: 'center',
         backgroundColor: CONSTANTS.backgroundColor
+    },
+    lastGames: {
+        flex: 1,
+        backgroundColor: 'red'
+    },
+    stats: {
+        flex: 1,
+        backgroundColor: 'yellow'
+    },
+    points: {
+        flex: 1,
+        backgroundColor: 'pink'
+    },
+    gamesData: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        backgroundColor: 'green'
     }
+
 });
 
 export default ProfileScreen;
