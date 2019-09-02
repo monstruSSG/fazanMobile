93b05272fb5f2de8cf195bb0f39cf9b645558630
monstruSSG <b_giurgi@yahoo.ro>
2019-09-02 21:49:39 +0300

    Now logged users comes from fakedata.json

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,3 +1,6 @@
 import * as USERS from './fakeData/users.json';
+import * as ME from './fakeData/me.json';
 
 export const getUsers = () => new Promise((resolve, reject) => resolve(Object.values(USERS)));
+
+export const getMe = () => new Promise((resolve, reject) => resolve(ME));
