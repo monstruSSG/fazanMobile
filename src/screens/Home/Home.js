3d68eac0e7f83cbf316da09c67a00d1cb893d659
monstruSSG <b_giurgi@yahoo.ro>
2019-09-08 17:24:33 +0300

    Added google auth, we have to make modal now for authentication

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -10,27 +10,28 @@ import BackgroundImg from '../../assets/back.png';
 import Logo from '../../assets/angryLogo.png';
 
 import { LoginManager, AccessToken } from 'react-native-fbsdk';
+import { GoogleSignin, GoogleSigninButton, statusCodes } from 'react-native-google-signin';
 
-const initUser = (token)  => {
+const initUser = (token) => {
     fetch('https://graph.facebook.com/v2.5/me?fields=email,name,friends&access_token=' + token)
-    .then((response) => response.json())
-    .then((json) => {
-      // Some user object has been set up somewhere, build that user here
-      user.name = json.name
-      user.id = json.id
-      user.user_friends = json.friends
-      user.email = json.email
-      user.username = json.name
-      user.loading = false
-      user.loggedIn = true
-      user.avatar = setAvatar(json.id)  
-      console.log(json, 'USER')    
-    })
-    .catch(() => {
-      reject('ERROR GETTING DATA FROM FACEBOOK')
-    })
-  }
-  
+        .then((response) => response.json())
+        .then((json) => {
+            // Some user object has been set up somewhere, build that user here
+            user.name = json.name
+            user.id = json.id
+            user.user_friends = json.friends
+            user.email = json.email
+            user.username = json.name
+            user.loading = false
+            user.loggedIn = true
+            user.avatar = setAvatar(json.id)
+            console.log(json, 'USER')
+        })
+        .catch(() => {
+            reject('ERROR GETTING DATA FROM FACEBOOK')
+        })
+}
+
 
 class HomeScreen extends Component {
     static navigationOptions = {
@@ -42,12 +43,76 @@ class HomeScreen extends Component {
     navigateSearchGameScreen = () => this.props.navigation.navigate('SearchGame');
     navigateProfileScreen = () => this.props.navigation.navigate('Profile');
 
-    
+    componentDidMount() {
+        GoogleSignin.configure({
+            //It is mandatory to call this method before attempting to call signIn()
+            scopes: ['https://www.googleapis.com/auth/drive.readonly'],
+            // Repleace with your webClientId generated from Firebase console
+            webClientId: '490019403002-olakiju29g4196h1e31gcs9gtkmmmuu6.apps.googleusercontent.com',
+        })
+        this._isSignedIn()
+    }
+
+    _isSignedIn = async () => {
+        const isSignedIn = await GoogleSignin.isSignedIn();
+        if (isSignedIn) {
+            alert('User is already signed in');
+            //Get the User details as user is already signed in
+            this._getCurrentUserInfo();
+        } else {
+            //alert("Please Login");
+            console.log('Please Login');
+        }
+        this.setState({ gettingLoginStatus: false });
+    };
+
+    _getCurrentUserInfo = async () => {
+        try {
+            const userInfo = await GoogleSignin.signInSilently();
+            console.log('User Info --> ', userInfo);
+            this.setState({ userInfo: userInfo });
+        } catch (error) {
+            if (error.code === statusCodes.SIGN_IN_REQUIRED) {
+                alert('User has not signed in yet');
+                console.log('User has not signed in yet');
+            } else {
+                alert("Something went wrong. Unable to get user's info");
+                console.log("Something went wrong. Unable to get user's info");
+            }
+        }
+    };
+
+    _signIn = async () => {
+        //Prompts a modal to let the user sign in into your application.
+        try {
+            await GoogleSignin.hasPlayServices({
+                //Check if device has Google Play Services installed.
+                //Always resolves to true on iOS.
+                showPlayServicesUpdateDialog: true,
+            });
+            const userInfo = await GoogleSignin.signIn();
+            console.log('User Info --> ', userInfo);
+            this.setState({ userInfo: userInfo });
+        } catch (error) {
+            console.log('Message', error.message);
+            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+                console.log('User Cancelled the Login Flow');
+            } else if (error.code === statusCodes.IN_PROGRESS) {
+                console.log('Signing In');
+            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                console.log('Play Services Not Available or Outdated');
+            } else {
+                console.log('Some Other Error Happened');
+            }
+        }
+    };
+
+
 
     async loginHandler() {
         try {
             let result = await LoginManager.logInWithPermissions(['public_profile'])
-            if(result.isCancelled) {
+            if (result.isCancelled) {
                 return alert('Canceled')
             }
 
@@ -96,6 +161,12 @@ class HomeScreen extends Component {
                                 style={styles.userProfile} />
                         </View>
                         <Button title="FACEBOOK LOGIN" onPress={this.loginHandler} />
+                        <GoogleSigninButton
+                            style={{ width: 312, height: 48 }}
+                            size={GoogleSigninButton.Size.Wide}
+                            color={GoogleSigninButton.Color.Light}
+                            onPress={this._signIn}
+                        />
                     </View>
                 </View>
             </ImageBackground>
