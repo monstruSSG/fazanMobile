385d82553638f360a84703dba6d2a15cc5fc0b66
monstruSSG <b_giurgi@yahoo.ro>
2019-10-18 23:08:47 +0300

    Modal closes now when pressing out

--- src/components/Modals/SideDrawer.js
+++ src/components/Modals/SideDrawer.js
@@ -10,58 +10,63 @@ import GreenButton from '../../assets/Buttons/greenLabel.png';
 
 const sideDrawer = props => (
     <Modal visible={props.isVisible} onRequestClose={props.onClose} animationType="fade" transparent={true}>
-        <View style={[styles.max, styles.container]}>
-            <ImageBackground source={Background} style={[styles.background]}>
-                <View style={[styles.max]}>
-                    <View style={[styles.head, styles.center]}>
-                        <View style={[styles.centerAndEnd, styles.half, styles.avatarPosition]}>
-                            <Image source={{ uri: props.pictureUrl }} style={{ width: 60, height: 60, borderRadius: 30 }} />
+        <View style={[styles.max, styles.container, {flexDirection: 'row'}]}>
+            <View style={[styles.background]}>
+                <ImageBackground source={Background} style={[styles.max]}>
+                    <View style={[styles.max]}>
+                        <View style={[styles.head, styles.center]}>
+                            <View style={[styles.centerAndEnd, styles.half, styles.avatarPosition]}>
+                                <Image source={{ uri: props.pictureUrl }} style={{ width: 60, height: 60, borderRadius: 30 }} />
+                            </View>
+                            <View style={[styles.centerAndEnd, styles.half]}>
+                                <CustomText normal>{props.username}</CustomText>
+                            </View>
                         </View>
-                        <View style={[styles.centerAndEnd, styles.half]}>
-                            <CustomText normal>{props.username}</CustomText>
+                        <View style={[styles.middle, styles.center]}>
+                            <View style={[styles.contentButtons, styles.centerAndEnd]}>
+                                <TouchableOpacity style={[styles.max]} onPress={props.goToClasament}>
+                                    <ImageBackground style={[styles.max, styles.center]} source={YellowButton} resizeMode='stretch'>
+                                        <CustomText normal style={[styles.textPosition]}>CLASAMENT</CustomText>
+                                    </ImageBackground>
+                                </TouchableOpacity>
+                            </View>
+                            <View style={[styles.contentButtons, styles.center]}>
+                                <TouchableOpacity style={[styles.max]} onPress={props.goToProfile}>
+                                    <ImageBackground style={[styles.max, styles.center]} source={YellowButton} resizeMode='stretch'>
+                                        <CustomText normal style={[styles.textPosition]}>PROFILE</CustomText>
+                                    </ImageBackground>
+                                </TouchableOpacity>
+                            </View>
+                            <View style={[styles.contentButtons, styles.center]}>
+                                <TouchableOpacity style={[styles.max]} onPress={props.goToHome}>
+                                    <ImageBackground style={[styles.max, styles.center]} source={YellowButton} resizeMode='stretch'>
+                                        <CustomText normal style={[styles.textPosition]}>ACASA</CustomText>
+                                    </ImageBackground>
+                                </TouchableOpacity>
+                            </View>
+                            <View style={[styles.contentButtons, styles.centerAndStart]}>
+                                <TouchableOpacity style={[styles.max]} onPress={props.onClose}>
+                                    <ImageBackground style={[styles.max, styles.center]} source={GreenButton} resizeMode='stretch'>
+                                        <CustomText normal style={[styles.textPosition]}>INCHIDE</CustomText>
+                                    </ImageBackground>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                        <View style={[styles.bottom, styles.centerAndEnd]}>
+                            <View style={[styles.contentButtons, { height: '50%' }, styles.centerAndStart]}>
+                                <TouchableOpacity style={[styles.max]} onPress={props.onLogout}>
+                                    <ImageBackground style={[styles.max, styles.center]} source={GreenButton} resizeMode='stretch'>
+                                        <CustomText normal style={[styles.textPosition]}>LOGOUT</CustomText>
+                                    </ImageBackground>
+                                </TouchableOpacity>
+                            </View>
                         </View>
                     </View>
-                    <View style={[styles.middle, styles.center]}>
-                        <View style={[styles.contentButtons, styles.centerAndEnd]}>
-                            <TouchableOpacity style={[styles.max]} onPress={props.goToClasament}>
-                                <ImageBackground style={[styles.max, styles.center]} source={YellowButton} resizeMode='stretch'>
-                                    <CustomText normal style={[styles.textPosition]}>CLASAMENT</CustomText>
-                                </ImageBackground>
-                            </TouchableOpacity>
-                        </View>
-                        <View style={[styles.contentButtons, styles.center]}>
-                            <TouchableOpacity style={[styles.max]} onPress={props.goToProfile}>
-                                <ImageBackground style={[styles.max, styles.center]} source={YellowButton} resizeMode='stretch'>
-                                    <CustomText normal style={[styles.textPosition]}>PROFILE</CustomText>
-                                </ImageBackground>
-                            </TouchableOpacity>
-                        </View>
-                        <View style={[styles.contentButtons, styles.center]}>
-                            <TouchableOpacity style={[styles.max]} onPress={props.goToHome}>
-                                <ImageBackground style={[styles.max, styles.center]} source={YellowButton} resizeMode='stretch'>
-                                    <CustomText normal style={[styles.textPosition]}>ACASA</CustomText>
-                                </ImageBackground>
-                            </TouchableOpacity>
-                        </View>
-                        <View style={[styles.contentButtons, styles.centerAndStart]}>
-                            <TouchableOpacity style={[styles.max]} onPress={props.onClose}>
-                                <ImageBackground style={[styles.max, styles.center]} source={GreenButton} resizeMode='stretch'>
-                                    <CustomText normal style={[styles.textPosition]}>INCHIDE</CustomText>
-                                </ImageBackground>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                    <View style={[styles.bottom, styles.centerAndEnd]}>
-                        <View style={[styles.contentButtons, { height: '50%' }, styles.centerAndStart]}>
-                            <TouchableOpacity style={[styles.max]} onPress={props.onLogout}>
-                                <ImageBackground style={[styles.max, styles.center]} source={GreenButton} resizeMode='stretch'>
-                                    <CustomText normal style={[styles.textPosition]}>LOGOUT</CustomText>
-                                </ImageBackground>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                </View>
-            </ImageBackground>
+                </ImageBackground>
+            </View>
+            <View style={{width: '40%', height: '100%'}}>
+                <TouchableOpacity style={styles.max} onPress={props.onClose}/>
+            </View>
         </View>
     </Modal>
 );
