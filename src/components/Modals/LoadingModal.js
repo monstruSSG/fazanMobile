c2528b8091b270c5ca3e9fac77cda930f78ffdf0
monstruSSG <b_giurgi@yahoo.ro>
2019-10-18 17:58:54 +0300

    Modified loading modal

--- src/components/Modals/LoadingModal.js
+++ src/components/Modals/LoadingModal.js
@@ -6,36 +6,63 @@ import CustomText from '../UI/Text/Text';
 import EmptyStar from '../../assets/Stuff/emptyStar.png';
 
 class LoadingModal extends Component {
-    spinValue = new Animated.Value(0);
+    first = new Animated.Value(0);
+    secound = new Animated.Value(0);
+    third = new Animated.Value(0)
 
     componentDidMount() {
         Animated.loop(
-            Animated.timing(this.spinValue, {
-                toValue: 1,
-                duration: 1500,
-                useNativeDriver: true
-            })
-        ).start(() => console.log('JERERE'));
+            Animated.sequence([
+                Animated.timing(this.first, {
+                    toValue: 1,
+                    duration: 300,
+                    useNativeDriver: true
+                }),
+                Animated.timing(this.secound, {
+                    toValue: 1,
+                    duration: 300,
+                    useNativeDriver: true
+                }),
+                Animated.timing(this.third, {
+                    toValue: 1,
+                    duration: 300,
+                    useNativeDriver: true
+                }),
+                Animated.parallel([
+                    Animated.timing(this.third, {
+                        toValue: 0,
+                        duration: 100,
+                        useNativeDriver: true
+                    }),
+                    Animated.timing(this.secound, {
+                        toValue: 0,
+                        duration: 100,
+                        useNativeDriver: true
+                    }),
+                    Animated.timing(this.first, {
+                        toValue: 0,
+                        duration: 100,
+                        useNativeDriver: true
+                    })
+                ])
+            ])
+        ).start(() => console.log('IN ANIMATION'));
     }
 
     render() {
-        const spinStyle = {
-            transform: [
-                {
-                    rotate: this.spinValue.interpolate({
-                        inputRange: [0, 1],
-                        outputRange: ['0deg', '360deg']
-                    })
-                }
-            ]
-        };
-
         return (
             <Modal visible={this.props.isVisible} onRequestClose={this.props.onClose} animationType={this.props.modalAnimation ? this.props.modalAnimation : 'slide'} transparent>
                 <View style={[styles.max, styles.center, styles.container]}>
-                    <Animated.View style={[{ width: '50%', height: '30%' }, spinStyle]}>
-                        <Image source={EmptyStar} resizeMode='contain' style={styles.max} />
-                    </Animated.View>
+                    <View style={[{ width: '80%', height: '15%', alignItems: 'center' }]}>
+                        <View style={[{ height: '50%', width: '100%' }, { alignItems: 'center', justifyContent: 'flex-end' }]}>
+                            <CustomText large>WAITING</CustomText>
+                        </View>
+                        <Animated.View style={[{ height: '20%', width: '100%' }, { alignItems: 'center', justifyContent: 'center', flexDirection: 'row' }]}>
+                            <Animated.View style={{ opacity: this.first }}><CustomText giant>.</CustomText></Animated.View>
+                            <Animated.View style={{ opacity: this.secound }}><CustomText giant>.</CustomText></Animated.View>
+                            <Animated.View style={{ opacity: this.third }}><CustomText giant>.</CustomText></Animated.View>
+                        </Animated.View>
+                    </View>
                 </View>
             </Modal >
         );
